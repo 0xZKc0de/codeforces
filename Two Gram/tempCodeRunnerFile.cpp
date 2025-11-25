7c7818c8fe4e6d296d150cde9ddbe864a5a9a5a5
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-11-25 01:55:59 +0100

    Now solve it with C++

---
+++ Two Gram/tempCodeRunnerFile.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+unordered_map<string, int> count_gram(const string& s, int n) {
+    
+    unordered_map<string, int> gram_counts;
+    for(int i = 0; i < n - 1; i++){
+        string g = s[i] + "" + s[i+1];
+        gram_counts[g]++;
+    }    
+    return gram_counts;
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int max_count = 0;
+    unordered_map<string, int> counts = count_gram(s, n);
+
+    string gram = "";
+    for (const auto& pair : counts) {
+        if (pair.second > max_count) {
+            max_count = pair.second;
+            gram = pair.first;
+        }
+    }
+    cout << gram << "\n";
+}
